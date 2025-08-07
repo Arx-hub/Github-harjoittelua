f78d98f6f5e318557b683aa86a4670c97bf4dae7
Arx-hub <arxhenda11@hotmail.com>
2025-08-07 21:46:14 +0300

    Create Program.cs
    creation of the cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Github_harjoittelua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
