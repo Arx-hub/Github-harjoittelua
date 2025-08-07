842176d60c27597fabd6b9a220d85a946bb6261a
Arx-hub <arxhenda11@hotmail.com>
2025-08-07 21:53:58 +0300

    Kirja-luokka
    Kirja-luokan lisäys

--- Program.cs
+++ Program.cs
@@ -9,4 +9,25 @@ namespace Github_harjoittelua
         {
         }
     }
+
+    class Kirja
+    {
+        private string nimi;
+        private string kirjoittaja;
+        private int julkaisuvuosi;
+        private string genre;
+
+        public Kirja(string nimi, string kirjoittaja, int julkaisuvuosi, string genre)
+        {
+            this.nimi = nimi;
+            this.kirjoittaja = kirjoittaja;
+            this.julkaisuvuosi = julkaisuvuosi;
+            this.genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"{nimi}, {kirjoittaja}, {julkaisuvuosi}, {genre}";
+        }
+    }
 }
